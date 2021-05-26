dca518b49734aef4a7c8493bee5bc2ab13b3b8ec
yasamnoya <yasamnono@gmail.com>
2021-05-26 14:50:01 +0800

    fix bugs

--- src/services/freeUdemyCourse.js
+++ src/services/freeUdemyCourse.js
@@ -2,28 +2,27 @@ const cron = require('node-cron')
 const axios = require('axios')
 
 const URL = 'https://vip.studycamp.tw/c/udemy/udemy-free-coupon-code/68/l/latest.json?ascending=false'
-const CHANNEL_ID = '846392787871596566'
+const UDEMY_CHANNEL_ID = process.env.UDEMY_CHANNEL_ID
 
 let latestTopicIdCache = 0
 
 async function startFreeUdemyCourse(client) {
-  const channel = client.channels.cache.get(CHANNEL_ID)
-  const schedule = '0 */10 * * * *'
+  const channel = client.channels.cache.get(UDEMY_CHANNEL_ID)
+  const schedule = '0 * * * * *'
 
   cron.schedule(schedule, async () => {
     console.log('Getting free udemy course')
-    const latestTopic = await getLatestTopic()
 
+    const latestTopic = await getLatestTopic()
     const previousMessage = await getPreviousMessage(channel)
+
+    if (!previousMessage) return sendLatestTopic(channel, latestTopic)
+
     const previousMessageTopicId = parseInt(previousMessage.content.split('/').slice(-1))
     const isNewTopic = previousMessageTopicId != latestTopic.id
 
-    if (!isNewTopic) {
-      console.log('There is no new free course\n')
-      return
-    }
+    if (!isNewTopic) return console.log('There is no new free course\n')
 
-    console.log('Sending new free course to channel')
     sendLatestTopic(channel, latestTopic)
   })
 }
@@ -49,6 +48,8 @@ async function getPreviousMessage(channel) {
 }
 
 function sendLatestTopic(channel, latestTopic) {
+  console.log('Sending new free course to channel')
+
   const title = latestTopic.title
   const url = `https://vip.studycamp.tw/t/topic/${latestTopic.id}`
   const message = `${title}\n${url}`
